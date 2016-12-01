4f360d09a3a891523897b30dfbec70ee621146d9
claudio@uerr.edu.br <claudio@uerr.edu.br>
2016-12-01 10:43:56 -0400

    Changing
    The 'installation' script was changed to make things simplified.

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 >Captures the response codes and customize the response page from the Apache HTTP 
 server using PHP, CSS and JavaScript. The file 'apache.conf' has the 
 configuration of the 'Alias' and of the codes you may need to make it work properly 
-(change it as you want). You should put a link to it into the 'mods-enabled' on your server. 
+(change it as you want). You should put a link to it into the 'mods-enabled' on your server.
--- install.sh
+++ install.sh
@@ -6,107 +6,125 @@
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
 # Short-Description: Simple configuration procedure
-# Description:       A simple script to start the needed configuration procedure
+# Description:       A simple script to start the needed configuration procedure 
 ### END INIT INFO
 
 #Cleaning up the screen
 clear;
 
-# Declaring the environment variables
-APACHE_DIR="/etc/apache2/";
-CONFIG_FILE="/usr/share/php/http-response/apache.conf";
-CONFIG_AVAILABLE_FILE=$APACHE_DIR"conf-available/http-response.conf";
-CONFIG_ENABLED_FILE=$APACHE_DIR"conf-enabled/http-response.conf";
-LINK_CREATE=$CONFIG_FILE" "$CONFIG_AVAILABLE_FILE;
-LINK_ACTIVATE=$CONFIG_AVAILABLE_FILE" "$CONFIG_ENABLED_FILE
 ACTION=$1;
 
+SYS_NAME="http-response";
+SYS_STATUS=`curl -sL -w "%{http_code} %{url_effective}\\n" "http://127.0.0.1/$SYS_NAME" -o /dev/null | awk '{print $1}'`;
+SYS_DIR="/opt/"$SYS_NAME"/";
+SYS_CONFIG_DIR=$SYS_DIR;
+SYS_APACHE_CONFIG_FILE=$SYS_CONFIG_DIR"apache.conf"
+SYS_SCRIPT_INSTALL=$SYS_CONFIG_DIR"install.sh"
+SYS_SCRIPT_INDEX=$SYS_DIR"index.php"
+
+APACHE_USER="www-data";
+APACHE_DIR="/etc/apache2/";
+APACHE_HTDOCS="/var/www/html/";
+APACHE_CONF_AVAILABLE_FILE=$APACHE_DIR"conf-available/"$SYS_NAME".conf ";
+APACHE_CONF_ENABLED_FILE=$APACHE_DIR"conf-enabled/"$SYS_NAME".conf ";
+
+SET_PERMISSIONS=(
+    $SYS_SCRIPT_INSTALL
+);
+PERMISSIONS=${#SET_PERMISSIONS[@]};
+
+SET_SYMBOLIC_LINKS=(
+    "$SYS_APACHE_CONFIG_FILE $APACHE_CONF_AVAILABLE_FILE"
+    "$APACHE_CONF_AVAILABLE_FILE $APACHE_CONF_ENABLED_FILE"
+);
+SYMBOLIC_LINKS=${#SET_SYMBOLIC_LINKS[@]};
+
+UNSET_SYMBOLIC_LINKS=(
+    "$APACHE_CONF_AVAILABLE_FILE"
+    "$APACHE_CONF_ENABLED_FILE"
+);
+SYMBOLIC_LINKS_UNSET=${#UNSET_SYMBOLIC_LINKS[@]};
+
 start(){
-        echo 'Checking symbolic links...';
-	if [ -e "$CONFIG_ENABLED_FILE" ] ; then
-            echo 'The link '$CONFIG_ENABLED_FILE' already exists.';
-            echo 'This program is terminated.';
-            exit 0;
-	else
-	echo 'Will create the first symbolic link' $CONFIG_AVAILABLE_FILE;
-		ln -s $LINK_CREATE;
-		if [ $? -eq 0 ] ; then
-                    echo Symbolic link $CONFIG_AVAILABLE_FILE successfully created!;
-		else
-                    echo 'Something went wrong. This program is terminated.';
-		exit 0;
-		fi
-	sleep 1;
-		#
-	echo 'Will create the second symbolic link' $CONFIG_ENABLED_FILE;
-		ln -s $LINK_ACTIVATE;
-		if [ $? -eq 0 ] ; then
-                    echo Symbolic link $CONFIG_ENABLED_FILE successfully created!;
-		else
-                    echo 'Something went wrong. This program is terminated.';
-		exit 0;
-		fi
-	sleep 1;
-		#
-	echo 'Will restart the Apache server';
-		service apache2 restart
-		if [ $? -eq 0 ] ; then
-                    echo The server was successfully restarted!;
-                    echo This program is terminated.;
-		else
-                    echo 'Something went wrong. This program is terminated.';
-		exit 0;
-		fi
-	sleep 1;
-	fi
+    # Sets the directories permissions
+    set_permissions;
+    # Creates all symbolic links
+    create_links;
+    # Restarts the Apache Server
+    service apache2 restart;
+    if [ $? -eq 0 ] ; then
+        echo "The server was successfully restarted!";
+    else
+        echo "Something went wrong while restarting server";
+        exit 1;
+    fi
+    echo "All done!";
 }
 
 stop(){
-	echo 'Checking symbolic links...';
-        if [ -e "$CONFIG_AVAILABLE_FILE" ] ; then
-            rm $CONFIG_AVAILABLE_FILE;
-            if [ $? -eq 0 ] ; then
-                echo Symbolic link $CONFIG_AVAILABLE_FILE successfully removed!;
-                #
-        echo 'Will remove the second symbolic link';
-                rm $CONFIG_ENABLED_FILE;
-                if [ $? -eq 0 ] ; then
-                    echo Symbolic link $CONFIG_ENABLED_FILE successfully removed!;
-                else
-                    echo 'Something went wrong. This program is terminated.';
-                exit 0;
-                fi
-        sleep 1;
-                #
-        echo 'Will restart the Apache server';
-                service apache2 restart
-                if [ $? -eq 0 ] ; then
-                    echo "The server was successful restarted!";
-                    echo "This program is terminated.";
-                else
-                    echo 'Something went wrong. This program is terminated.';
-                    exit 0;
-                fi
-        sleep 1;
-                exit 0;
-            fi
-            else
-                echo "File '"$CONFIG_AVAILABLE_FILE"' not found!";
-                if [ $ACTION = 'restart' ] ; then
-                    start;
-                    exit 0;
-                fi
-                echo 'This program is terminated.';
-            exit 0;
-        fi
-        sleep 1;
+    if [ $SYS_STATUS != 200 ] ; then
+        echo "The system is already offline. You should use the 'start' parameter.";
+        echo "We'll do this for you right now. Wait, please...";
+        sleep 5;
+        start;
+        exit 0;
+    fi
+    # Unsets the permissions of the directories
+    unset_permissions;
+    # Removes all symbolic links
+    remove_links;
+    # Restarts the Apache Server
+    service apache2 restart;
+    if [ $? -eq 0 ] ; then
+        echo "The server was successfully restarted!";
+    else
+        echo "Something went wrong while restarting server";
+        exit 1;
+    fi
+    echo "All done!";
+}
 
+create_links(){
+    echo "Creating all symbolic links...";
+    for (( i=0; i<$SYMBOLIC_LINKS; i++)); do
+        VAR=${SET_SYMBOLIC_LINKS[${i}]};
+        ln -s $VAR;
+    done;
+    sleep 2;
+}
+
+remove_links(){
+    echo "Removing all symbolic links...";
+    for (( i=0; i<$SYMBOLIC_LINKS_UNSET; i++)); do
+        VAR=${UNSET_SYMBOLIC_LINKS[${i}]};
+        rm -r $VAR;
+    done;
+    sleep 2;
+}
+
+set_permissions(){
+    echo "Setting permissions...";
+    for (( i=0; i<$PERMISSIONS; i++)); do
+        VAR=${SET_PERMISSIONS[${i}]};
+        chmod +x $VAR;
+        echo "Setting execution permission in '$VAR'...";
+    done;
+    sleep 2;
+}
+
+unset_permissions(){
+    echo "Unsetting permissions...";
+    for (( i=0; i<$PERMISSIONS; i++)); do
+        VAR=${SET_PERMISSIONS[${i}]};
+        chmod 644 $VAR;
+        echo "Unsetting execution permission in '$VAR'...";
+    done;
+    sleep 2;
 }
 
 case "$1" in
 "start") start ;;
 "stop") stop ;;
-"restart") stop; start ;;
+"restart") stop ; start ;;
 *) echo "Use 'start', 'stop' or 'restart' commands!";
 esac
-
